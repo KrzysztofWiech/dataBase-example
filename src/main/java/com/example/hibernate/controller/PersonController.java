8ed5ec54b589daa8337e27fb5141c9434e6ca195
KrzysztofWiech <bystrow7@gmail.com>
2019-03-12 16:31:20 +0100

    second commit

--- src/main/java/com/example/hibernate/controller/PersonController.java
+++ src/main/java/com/example/hibernate/controller/PersonController.java
@@ -1,10 +1,7 @@
 package com.example.hibernate.controller;
-
 import com.example.hibernate.model.Person;
 import com.example.hibernate.repozytory.PersonRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -13,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.validation.Valid;
 
 @Controller
